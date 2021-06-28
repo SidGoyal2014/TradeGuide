598dee42cd83dbfe4ebf45039b3a98d47386956a
Siddharth Goyal <sidgoyal2014@gmail.com>
2021-06-28 18:44:20 +0530

    Started the tradeguide... initial setup & startup

---
+++ index.js
@@ -0,0 +1,32 @@
+const mongoclient = require('mongodb');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const port = 3000;
+
+// set the name of the website
+var config = fs.readFileSync("./package.json", "utf8", (err,jsonstring)=>{
+});
+
+config = JSON.parse(config);
+
+const website_name = config.name;
+
+// Initializing the app
+const app = express();
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static'));
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname,'views'));
+
+// ENDPOINTS
+app.get('/', (req,res)=>{
+    res.status(200).render('index.pug', {title:website_name,data:null});
+});
+
+app.listen(port,()=>{
+    console.log("The server is running at port : ", port);
+});
